82cfdb3dbc450150de0a4912f5e1e7e2d1015d55
unknown <rithwhickpraharsha@gmail.com>
2024-01-16 13:17:40 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,15 @@ const secret_model = require("./models.js/secret");
 const {userParse,secretParse} = require("./inputEvalution");
 const dotenv = require('dotenv');
 dotenv.config();
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+  
 app.use(bodyParser.json());
 
 
