08f227c8b2fa29f3103d13f4ee8d1d5b3de1f8c6
unknown <rithwhickpraharsha@gmail.com>
2024-01-16 11:24:13 +0530

    deploying

--- secret/src/components/Signup.js
+++ secret/src/components/Signup.js
@@ -12,7 +12,7 @@ export default function Signup(){
       if(Confirm === Password){
        
         try{
-           const token =  await axios.post('https://secret-app-gamma.vercel.app/user/signup',{user:email,password:Password});
+           const token =  await axios.post('http://localhost:5000/user/signup',{user:email,password:Password});
            localStorage.setItem('secret-app',token.data);
            navigate('/App');
            
