e19167fbc29dc668cd0a63a642ab86e83ac60917
unknown <rithwhickpraharsha@gmail.com>
2024-01-16 21:09:34 +0530

    Added Strict Mode

--- secret/src/index.js
+++ secret/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,8 +9,10 @@ import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<StrictMode> 
     <App />
+</StrictMode>
+    
  
 );
 
